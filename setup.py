dbdbc400da5d2a42059e93826c1e1dcd8d5b0827
Mikhail Erofeev <merofeev@graphics.cs.msu.ru>
2018-10-05 17:47:58 +0300

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='docker-windows-volume-watcher',
-      version='1.0.8',
+      version='1.1.0',
       packages=find_packages(),
       entry_points={
           'console_scripts': ['docker-volume-watcher=docker_volume_watcher.cli:main'],
